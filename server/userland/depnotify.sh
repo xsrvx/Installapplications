50744a96c3b38dc5e083f2a0c62f9531efb34a26
xsrvx <pintom@flaglerschools.com>
2020-05-21 12:55:13 -0400

    Update depnotify.sh

--- server/userland/depnotify.sh
+++ server/userland/depnotify.sh
@@ -11,10 +11,6 @@
 #  Updated: 05/21/20
 #
 
-# DEPNotify location
-DNApp="/Applications/Utilities/DEPNotify.app"
-# Get the logged in user (should be admin)
-DN_User=$(stat -f %Su "/dev/console")
 # Command file
 DNLog="/var/tmp/depnotify.log"
 
@@ -33,6 +29,3 @@ echo "Command: Image: /usr/local/.install/shaded-district-logo.png" >> $DNLog
 echo "Command: Status: Preparing..." >> $DNLog
 echo "Command: Determinate: 4" >> $DNLog
 echo "Command: MainTitle: Welcome to Flagler County Public Schools!" >> $DNLog
-
-# Run DEPNotify
-sudo -u "$DN_User" $DNApp/Contents/MacOS/DEPNotify -fullScreen &
