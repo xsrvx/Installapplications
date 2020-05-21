6cb8ef3e92b2c7f160c8f0fd7b2bc9a5e97ff765
xsrvx <pintom@flaglerschools.com>
2020-05-21 11:40:01 -0400

    Update postinstall2.sh

--- server/userland/postinstall2.sh
+++ server/userland/postinstall2.sh
@@ -61,32 +61,6 @@ mv /tmp/shaded-district-logo.png /usr/local/.install/
 chmod -R 755 /usr/local/.install
 chown -R root:wheel /usr/local/.install
 
-# DEPNotify location
-DNApp="/Applications/Utilities/DEPNotify.app"
-# Get the logged in user (should be admin)
-DN_User=$(stat -f %Su "/dev/console")
-# Command file
-DNLog="/var/tmp/depnotify.log"
-
-# Remove previous DEPNotify command file if it exists
-if [[ -e /var/tmp/depnotify.log ]]; then
-	rm -rf $DNLog
-	# create new file
-	touch $DNLog
-else
-	# create new file
-	touch $DNLog
-fi
-
-# Prepare DEPNotify
-echo "Command: Image: /usr/local/.install/shaded-district-logo.png" >> $DNLog
-echo "Command: Status: Preparing..." >> $DNLog
-echo "Command: Determinate: 4" >> $DNLog
-echo "Command: MainTitle: Welcome to Flagler County Public Schools!" >> $DNLog
-
-# Run DEPNotify
-sudo -u "$DN_User" $DNApp/Contents/MacOS/DEPNotify -fullScreen &
-
 # Disable root login by setting root's shell to /usr/bin/false
 # To revert it back to /bin/sh, run the following command:
 # /usr/bin/dscl . -change /Users/root UserShell /usr/bin/false /bin/sh
